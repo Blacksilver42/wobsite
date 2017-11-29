090419a0ea7f18a4251018bb10d83101761d1cae
Blacksilver42 <blacksilverck35@gmail.com>
2017-11-29 21:25:26 +0000

    more info on the kcp/howto page

--- kcp/howto/index.php
+++ kcp/howto/index.php
@@ -17,10 +17,27 @@ include "/var/www/html/kcp/top/top.php"
 
 <div id="main">
 	<ul>
-		<li>New players...
+		<li>New players
 			<ul>
 				<li>Create a corp: <span class="code">/corp create <u>NAME</u></span>. Set the tag with <span class="code">/corp tag <u>TAG</u> <u>NAME</u></li>
 				<li>Join a corp: <span class="code">/corp join <u>NAME</u></span></li>
+			</ul>
+		</li>
+		<li>LPs
+			<ul>
+				<li>Build a rocket.</li>
+				<li>PM @icantmakemodels with a <a href="http://kerbalx.com">KerbalX</a> link</li>
+				<li>Negotiate with a PP for something to launch</li>
+				<li>I'll launch the rocket</li>
+				<li>PROFIT!</li>
+			</ul>
+		</li>
+		<li>PPs
+			<ul>
+				<li>Build a payload</li>
+				<li>Negotiate with a LP for a launch</li>
+				<li>Get paid by GOV (or other corps!) for use of the sat</li>
+				<li>PROFIT!</li>
 			</ul>
 		</li>
 	</ul>
