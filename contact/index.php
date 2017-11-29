c11c24eb5b38146b6de107e2ea545cb302bb9d2e
Blacksilver42 <blacksilverck35@gmail.com>
2017-11-29 20:59:55 +0000

    create contact/

---
+++ contact/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+	<title>Blacksilver &raquo; Contact</title>
+	<link rel="stylesheet" type="text/css" href="/master.css" />
+</head>
+<body>
+
+	<?php include "contact.html" ?>
+
+	<a href="..">&laquo; back</a>
+</body>
