ea56712c816a6cab889433b839651ccafebb4314
Blacksilver42 <blacksilverck35@gmail.com>
2017-12-06 15:29:41 +0000

    Change license to cc-by-nc

--- index.php
+++ index.php
@@ -160,11 +160,7 @@
 
 
 <div id="cc" >
-	<a	rel="license" 
-		href="http://creativecommons.org/licenses/by/2.5/ca/"
-		title="This work is licensed under a Creative Commons Attribution 2.5 Canada License.">
-		<img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by/2.5/ca/88x31.png" />
-	</a>
+	<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Blacksilver.xyz</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://blacksilver.xyz" property="cc:attributionName" rel="cc:attributionURL">Connor "Blacksilver" [REDACTED]</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
 </div>
 
 <center>
