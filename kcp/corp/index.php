01b6019e1f6c7622dfabe6f2d2da2c48e3d49d2b
Blacksilver42 <blacksilverck35@gmail.com>
2017-12-10 15:49:48 +0000

    Modified to use crafts.css...
    instead of it's own style.

--- kcp/corp/index.php
+++ kcp/corp/index.php
@@ -12,7 +12,7 @@
 	
 	<title>KCP &raquo; Corp index</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="/kcp/crafts.css">
 <style>
 	
 </style>
