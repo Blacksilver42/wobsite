95ed90ad89f0513e310c975500f1011b73b980bc
Blacksilver42 <blacksilverck35@gmail.com>
2017-12-16 18:14:39 +0000

    kcp/service

---
+++ kcp/service/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<?php include "/var/www/html/mobile.php" ?>
+
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
+	<title>KCP &raquo; Service</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
+	<script src="/git/kcp-scripts/service.js"></script> 
+
+
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" type="text/css" href="/kcp/crafts.css" />
+	
+	<?php include '/var/www/html/kcp/top/head.php'; ?>
+</head>
+<body>
+<?php
+	$pagename = "Service craft";
+	include "/var/www/html/kcp/top/top.php";
+?>
+
+
+
+<table id="table">
+	<tr>
+		<th>Tag</th>
+		<th>Name</th>
+		<th>Cost</th>
+		<th>Status</th>
+	</tr>
+</table>
+
+</body>
