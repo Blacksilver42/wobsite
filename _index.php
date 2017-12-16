74825a2f3e522fa66b5f0b2f3b08bc0ce9da00c6
Blacksilver42 <blacksilverck35@gmail.com>
2017-12-16 18:12:09 +0000

    BTN

---
+++ _index.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html>
+<head>
+	<title>#INTERNETEMERGENCY | Blacksilver's wobsite</title>
+	
+	
+	<!-- ===== BTFN ===== -->
+	
+	<script>
+		var _bftn_options = {
+			theme: 'glitch'
+		}
+	</script>
+	<script src="https://widget.battleforthenet.com/widget.js" async></script>
+	
+	
+	
+	<!-- ===== META TAGS ===== -->
+	
+	<meta name="description" content="Hi! I'm Blacksilver, I write things on the internet sometimes. Stay a while and listen!">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="Blacksilver, blacksilver.xyz">
+	<meta name="author" content="Connor 'Blacksilver' [REDACTED]">
+	
+	
+	
+	<!-- ===== CSS ===== -->
+
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" type="text/css" href="master.css" />
+	<link rel="stylesheet" type="text/css" href="top.css" />
+	<link rel="stylesheet" type="text/css" href="hint.css" />
+	
+	
+	
+	<!-- ===== SCRIPTS ==== -->
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
+	<script src="/git/website-scripts/wittyhint.js"></script>
+	<script src="/git/website-scripts/hideico.js"></script>
+	<script src="/git/website-scripts/misc.js"></script>
+	<script src="/THESYSTEM.js"></script>
+	
+<!--
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script>
+	  (adsbygoogle = window.adsbygoogle || []).push({
+	    google_ad_client: "ca-pub-9386404391656083",
+	    enable_page_level_ads: true
+	  });
+	</script>
+-->
+</head>
+<body>
+<p class="hideme" style="color: red; font-weight: bold;">
+	If you're reading this, your browser failed to load the CSS. Try refreshing.
+	<br>
+	If you've tried that, try ctrl+F5.
+	<br>
+	Still not working? The problem might be on my end, so wait a few minutes till I find the problem and fix it.
+</p>
+
+
+<div id="top">
+	<div id="top1">
+		<img src="https://i.imgur.com/EjwMRn8.png" alt="Scary trefoil" title="RADical Industries" class="mhide"></img>
+		<h1>
+			Hello.
+		</h1>
+		<h2>
+			It appears you have found my <strike>website</strike> wobsite.
+		</h2>
+		<h3>
+			Good job!
+		</h3>
+	</div>
+	<div id="split"></div>
+	<div id="top2">
+		<b>Hi! I'm Connor "Blacksilver" <span class="redacted">[REDACTED]</span>.</b>
+		<br>
+		<p>I make stuff on the internet sometimes.</p>
+	</div>
+</div>
+
+<!-- END TOP / BEGIN MAIN ===================================================== -->
+
+
+
+<div id="main">
+	<p id="witty-hint" title="Press `h` for a new one.">
+		Generating witty loading hints...
+	</span>
+	
+	
+	
+	<div class="type1">
+		<h2>Links</h2>
+		<p>More below</p>
+		<ul>
+			<li><a href="/kcp"		>Kerbal Capitalism Program</a></li>
+			<li><a href="/projects"	>Projects</a></li>
+			<li><a href="/blag"		>Blag</a></li>
+			<li><a href="/science"	>SCIENCE</a></li>
+			<li><a href="/ssb"		>Solving stratzenblitz</a></li>
+		</ul>
+	</div>
+	
+	
+	
+<?php include "/var/www/html/contact/contact.html" ?>
+
+
+	<div id="credits" class="type1">
+		<h3>Website credits</h3>
+		<table>
+			<tr>
+				<th>Blacksilver</th>
+				<td>Everything</td>
+			</tr>
+		</table>
+	</div>
+
+	<div class="type1">
+		<h3>More links</h3>
+		<ul>
+			<li><a href="/fortune">Fortune</a></li>
+			<li><a href="/attacks">Attacks</a></li>
+			<li><a href="/trail">A trail of breadcrumbs...</a></li>
+		</ul>
+	</div>
+
+	<p>
+		Turns out, .xyz is a real TLD; you can get your own at <a href="http://gen.xyz">gen.xyz</a>. This server is hosted on a Google Cloud Platform VM instance. Please don't DDoS it ;)
+	</p>
+
+	<p>
+		Want to send me money? Umm...
+		<a
+		  href="https://www.paypal.me/Blacksilver42"
+		  rel="nofollow"
+		  title="Thanks! <3">
+			Okay, I'm not complaining.
+		</a>
+	</p>
+
+	
+	<div id="ad" title="Okay, there's one in /wip when I'm working on this page, but it doesn't count.">
+		<h4>
+			The only ad on the site
+		</h4>
+		
+		<p>
+			Hosting and the domain don't pay for themselves. This is the only ad on the site.<br>
+			If you click on it, you will have payed the hosting for your visit. (and then some!)
+		</p>
+		
+		<p><em>If you're reading this, there's no ad to show. Woot!</em></p>
+	</div>
+</div>
+
+
+<div id="cc" >
+	<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Blacksilver.xyz</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://blacksilver.xyz" property="cc:attributionName" rel="cc:attributionURL">Connor "Blacksilver" [REDACTED]</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
+</div>
+
+<center>
+<a href="http://battleforthenet.com"><img src="https://www.battleforthenet.com/images/banner/728x80.png" style="width: 50%"></a>
+</center>
+</body>
+</html>
